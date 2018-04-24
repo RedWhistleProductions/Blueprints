c8a476597f6a22e7d708a48904cffb83c8d3c774
James <J_W_Snyder@yahoo.com>
2018-04-24 16:33:58 +0000

    4-24-18

--- test.py
+++ test.py
@@ -20,5 +20,9 @@ def Home():
     return output
 
 if __name__ == "__main__":
+    import platform
     app.secret_key = 'super_secret_key'
-    app.run(debug= True)
+    if platform.system() == "Linux":
+        app.run(host = "0.0.0.0", port= 8000, debug= True)
+    else:
+        app.run(port=8000, debug=True)
